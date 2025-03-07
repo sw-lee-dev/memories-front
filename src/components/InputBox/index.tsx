173fa8cfe07b590f1a0beff51dc5fb1d1c7b795f
sw-lee-dev <lsw6212@naver.com>
2025-03-07 12:40:25 +0900

    2025.03.07 12:50

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './style.css';
 
+// interface: 공통 인풋 박스 컴포넌트 속성 //
+
+interface Props {
+  label: string;
+  value: string;
+  placeholder: string;
+  type: 'text' | 'password';
+  buttonName: string;
+  message: string;
+
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
 // component: 공통 인풋 박스 컴포넌트 //
-export default function InputBox() {
+export default function InputBox(props: Props) {
+
+  const { label, value, placeholder, type, buttonName, message } = props;
+  const { onChange } = props;
 
   // render: 공통 인풋 박스 컴포넌트 렌더링 //
   return (
     <div className='input-box'>
-      <div className='label'>아이디</div>
+      <div className='label'>{label}</div>
       <div className='input-contents'>
         <div className='input-area'>
-          <input placeholder='아이디를 입력하세요.' />
-          <div className='button second'>중복 확인</div>
+          <input type={type} value={value} placeholder={placeholder} onChange={onChange} />
+          <div className='button second'>{buttonName}</div>
         </div>
-        <div className='message success'>사용 가능한 아이디 입니다.</div>
+        <div className='message success'>{message}</div>
       </div>
     </div>
   )
