e940a36acf74b74734d243fa06c5b82707f07ecd
sw-lee-dev <lsw6212@naver.com>
2025-03-10 10:52:28 +0900

    2025.03.10 10:50

---
+++ src/views/Auth/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+// component: 회원가입 컴포넌트 //
+export default function SignUp() {
+
+  // render: 회원가입 컴포넌트 렌더링 //
+  return (
+    <div>SignUp</div>
+  )
+}
