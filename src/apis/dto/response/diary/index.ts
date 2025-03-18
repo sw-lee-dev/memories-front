190c842a9520597ac227e231b2a2fab677d9cbc7
sw-lee-dev <lsw6212@naver.com>
2025-03-18 15:50:44 +0900

    2025.03.18 15:50

---
+++ src/apis/dto/response/diary/index.ts
@@ -0,0 +1,7 @@
+import GetMyDiaryResponseDto from './get-my-diary.response.dto';
+import GetDiaryReponseDto from './get-diary.response.dto';
+
+export type {
+  GetMyDiaryResponseDto,
+  GetDiaryReponseDto
+}
