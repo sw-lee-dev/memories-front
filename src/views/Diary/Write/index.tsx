bc26c56a76aff80a6c7ec1b8d0932220d905120f
sw-lee-dev <lsw6212@naver.com>
2025-03-19 17:46:19 +0900

    2025.03.19 17:50

--- src/views/Diary/Write/index.tsx
+++ src/views/Diary/Write/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css';
+import TextEditor from 'src/components/TextEditor';
 
 // component: 일기 작성 화면 컴포넌트 //
 export default function DiaryWrite() {
+
+  const [content, setContent] = useState<string>('');
+
+  const onContentChangeHandler = (content: string) => {
+    setContent(content);
+  };
   
   // render: 일기 작성 화면 컴포넌트 렌더링 //
   return (
@@ -56,6 +63,7 @@ export default function DiaryWrite() {
           </div>
           <div className='input-column-box'>
             <div className='title'>내용</div>
+            <TextEditor content={content} setContent={onContentChangeHandler} />
           </div>
         </div>
       </div>
