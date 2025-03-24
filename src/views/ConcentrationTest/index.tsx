3178e2704d8479cace561e962555c9bfc1e2dfaf
sw-lee-dev <lsw6212@naver.com>
2025-03-24 12:52:46 +0900

    2025.03.24 12:50

--- src/views/ConcentrationTest/index.tsx
+++ src/views/ConcentrationTest/index.tsx
@@ -1,18 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './style.css';
 import { CONCENTRATION_DESCRIPTION } from 'src/constants';
+import { useConcentrationTestStore } from 'src/stores';
+
+// variable: 전체 시간 (60초) //
+const TOTAL_TIME = 60 * 1000;
+// variable: 별 표시 시간 (0.25초) //
+const STAR_TIME = 250;
+// variable: 별 표시 횟수 (20번) //
+const STAR_COUNT = 20;
 
 // component: 집중력 검사 화면 컴포넌트 //
 export default function ConcentrationTest() {
 
   // state: 검사 시작 여부 상태 //
   const [isStarted, setStarted] = useState<boolean>(false);
+  // state: 검사 종료 여부 상태 //
+  const [isFinish, setFinish] = useState<boolean>(false);
+  // state: 별 표시 여부 상태 //
+  const [isStarVisible, setStarVisible] = useState<boolean>(false);
+  // state: 집중력 검사 결과 상태 //
+  const { measurementScore, errorCount, increaseMeasurementScore, increaseErrorCount } = useConcentrationTestStore();
 
   // event handler: 검사 시작 버튼 클릭 이벤트 처리 //
   const onStartClickHandler = () => {
     setStarted(true);
   };
+
+  // event handler: 아이콘 클릭 버튼 이벤트 처리 //
+  const onIconClickHandler = () => {
+    if (!isStarted || isFinish) return;
+    if (isStarVisible) {
+      increaseMeasurementScore();
+      setStarVisible(false);
+    }
+    else {
+      increaseErrorCount();
+    }
+  };
+
+  // effect: 검사 시작 상태가 변경될 시 실행할 함수 //
+  useEffect(() => {
+    if (isStarted) {
+      increaseMeasurementScore();
+      increaseErrorCount();
+
+      setTimeout(() => {
+        setFinish(true);
+        setStarted(false);
+      }, TOTAL_TIME);
+
+      setInterval(() => {
+        setStarVisible(true);
+
+        setTimeout(() => {
+          setStarVisible(false);
+        },STAR_TIME);
+
+      }, Math.floor(TOTAL_TIME / STAR_COUNT));
+    }
+  }, [isStarted]);
 
   // render: 집중력 검사 화면 컴포넌트 렌더링 //
   return (
@@ -28,14 +76,17 @@ export default function ConcentrationTest() {
             <div className='result-row'>
               <div className='result-box'>
                 <div className='title'>성공</div>
-                <div className='success'>0/20</div>
+                <div className='success'>{measurementScore}/20</div>
               </div>
               <div className='result-box'>
                 <div className='title'>오류</div>
-                <div className='error'>0</div>
+                <div className='error'>{errorCount}</div>
               </div>
             </div>
-            <div className='rectangle'></div>
+            {isStarVisible ? 
+            <div className='star' onClick={onIconClickHandler}></div> :
+            <div className='rectangle' onClick={onIconClickHandler}></div>
+            }
           </div> :
           <div className='button primary middle' onClick={onStartClickHandler}>검사 시작</div>          
           }
