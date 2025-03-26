6ae0a204d318ed5ee8cf2bfa96732a28bc699102
sw-lee-dev <lsw6212@naver.com>
2025-03-26 14:53:34 +0900

    2025.03.26 14:50

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -9,6 +9,8 @@ import { GetSignInUserResponseDto } from './dto/response/user';
 import { PostConcentrationRequestDto, PostMemoryRequestDto } from './dto/request/test';
 import { GetConcentrationResponseDto, GetMemoryResponseDto, GetRecentlyConcentrationResponseDto, GetRecentlyMemoryResponseDto } from './dto/response/test';
 import { PatchUserRequestDto } from './dto/request/user';
+import { GetWayRequestBodyDto } from './dto/request/openai';
+import { GetWayResponseDto } from './dto/response/openai';
 
 // variable: URL 상수 //
 const API_DOMAIN = process.env.REACT_APP_API_DOMAIN;
@@ -41,6 +43,10 @@ const GET_MEMORY_URL = `${TEST_MODULE_URL}/memory`;
 const GET_CONCENTRATION_URL = `${TEST_MODULE_URL}/concentration`;
 const GET_RECENTLY_MEMORY_URL = `${TEST_MODULE_URL}/memory/recently`;
 const GET_RECENTLY_CONCENTRATION_URL = `${TEST_MODULE_URL}/concentration/recently`;
+
+const OPEN_AI_MODULE_URL = `${API_DOMAIN}/api/v1/open-ai`;
+
+const GET_WAY_URL = `${OPEN_AI_MODULE_URL}/way`;
 
 const FILE_UPLOAD_URL = `${API_DOMAIN}/file/upload`;
 
@@ -179,6 +185,14 @@ export const getRecentlyMemoryRequest = async (accessToken: string) => {
 export const getRecentlyConcentrationRequest = async (accessToken: string) => {
   const responseBody = await axios.get(GET_RECENTLY_CONCENTRATION_URL, bearerAuthorization(accessToken))
     .then(responseSuccessHandler<GetRecentlyConcentrationResponseDto>)
+    .catch(responseErrorHandler);
+  return responseBody;
+};
+
+// function: get way API 요청 함수 //
+export const getWayRequest = async (requestBody: GetWayRequestBodyDto, accessToken: string) => {
+  const responseBody = await axios.post(GET_WAY_URL, requestBody, bearerAuthorization(accessToken))
+    .then(responseSuccessHandler<GetWayResponseDto>)
     .catch(responseErrorHandler);
   return responseBody;
 };
