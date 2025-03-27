c1116cd85f59763986a17d450dedc36176aa2542
sw-lee-dev <lsw6212@naver.com>
2025-03-27 11:51:33 +0900

    2025.03.27 11:50

--- src/apis/dto/response/diary/index.ts
+++ src/apis/dto/response/diary/index.ts
@@ -1,9 +1,11 @@
 import GetMyDiaryResponseDto from './get-my-diary.response.dto';
 import GetDiaryResponseDto from './get-diary.response.dto';
 import GetEmpathyResponseDto from './get-empathy.response.dto';
+import GetCommentResponseDto from './get-comment.response.dto';
 
 export type {
   GetMyDiaryResponseDto,
   GetDiaryResponseDto,
-  GetEmpathyResponseDto
+  GetEmpathyResponseDto,
+  GetCommentResponseDto
 }
